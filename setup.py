fe5ffa11f827c4c35519289edd3711e1d6cbcf56
Jonathan Huot <jonathan.huot@gmail.com>
2017-02-09 00:28:17 +0100

    Fixed setup.py for pip install

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "plupload",
-    version = "1.0",
-    author = "Jonathan Huot",
-    author_email = "jonathan.huot@gmail.com",
-    description = ("use plupload javascript library with python web apps based on bottlepy"),
-    license = "MIT",
-    keywords = "bottle web wsgi js plupload",
-    url = "https://github.com/JonathanHuot/bottlepy-plupload",
+    name="plupload",
+    version="1.0",
+    author="Jonathan Huot",
+    author_email="jonathan.huot@gmail.com",
+    description=("use plupload javascript library with python web apps based on bottlepy"),
+    license="MIT",
+    keywords="bottle web wsgi js plupload",
+    url="https://github.com/JonathanHuot/bottlepy-plupload",
     packages=['plupload'],
     long_description=read('README.md'),
     classifiers=[
@@ -21,6 +22,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Framework :: Bottle',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
